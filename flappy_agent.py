b585c0b1b4e22057ba0df47407a2ff77f16fbe80
Ketill Antoníus Ágústsson <ketill@ozio.is>
2018-12-12 20:51:30 +0000

    code cleanup

--- flappy_agent.py
+++ flappy_agent.py
@@ -22,61 +22,10 @@ import tensorflow as tf
 reward_structures = {
     "basicFlappyAgent": {"positive": 1.0, "tick": 0.0, "loss": -5.0},
     "improvedFlappyAgent": {"positive": 1.0, "tick": 0.1, "loss": -1.0},
-    "game": {"positive": 1.0, "negative": 0.0, "tick": 0.0, "loss": 0.0, "win": 0.0}
+    "ActualGame": {"positive": 1.0, "negative": 0.0, "tick": 0.0, "loss": 0.0, "win": 0.0}
 }
 
-
-class FlappyAgent:
-    def __init__(self):
-        # TODO: you may need to do some initialization for your agent here
-        return
-
-    def reward_values(self):
-        """ returns the reward values used for training
-
-            Note: These are only the rewards used for training.
-            The rewards used for evaluating the agent will always be
-            1 for passing through each pipe and 0 for all other state
-            transitions.
-        """
-        return reward_structures["improvedFlappyAgent"]
-
-    def observe(self, s1, a, r, s2, end):
-        """ this function is called during training on each step of the game where
-            the state transition is going from state s1 with action a to state s2 and
-            yields the reward r. If s2 is a terminal state, end==True, otherwise end==False.
-
-            Unless a terminal state was reached, two subsequent calls to observe will be for
-            subsequent steps in the same episode. That is, s1 in the second call will be s2
-            from the first call.
-            """
-        # TODO: learn from the observation
-        return
-
-    def training_policy(self, state):
-        """ Returns the index of the action that should be done in state while training the agent.
-            Possible actions in Flappy Bird are 0 (flap the wing) or 1 (do nothing).
-
-            training_policy is called once per frame in the game while training
-        """
-        print("state: %s" % state)
-        # TODO: change this to to policy the agent is supposed to use while training
-        # At the moment we just return an action uniformly at random.
-        return random.randint(0, 1)
-
-    def policy(self, state):
-        """ Returns the index of the action that should be done in state when training is completed.
-            Possible actions in Flappy Bird are 0 (flap the wing) or 1 (do nothing).
-
-            policy is called once per frame in the game (30 times per second in real-time)
-            and needs to be sufficiently fast to not slow down the game.
-        """
-        print("state: %s" % state)
-        # TODO: change this to to policy the agent has learned
-        # At the moment we just return an action uniformly at random.
-        return random.randint(0, 1)
-
-class FlappyDeepQAgent(FlappyAgent):
+class FlappyDeepQAgent:
     def __init__(self, reward_values=reward_structures["improvedFlappyAgent"], learning_rate=1e-5, discount_factor = 0.95, initial_epsilon=1, epsilon_decay_rate=0.9998, mini_epochs=7, training_episodes=20000, batch_size=32, reload_model=False, reload_weights=False):
         self.rewards = reward_values
         self.learningRate = learning_rate
@@ -104,20 +53,25 @@ class FlappyDeepQAgent(FlappyAgent):
 
 
     def reward_values(self):
+        """ returns the reward values used for training
+
+            Note: These are only the rewards used for training.
+            The rewards used for evaluating the agent will always be
+            1 for passing through each pipe and 0 for all other state
+            transitions.
+        """
         return self.rewards
 
     def observe(self, s1, a, r, s2, isEnd):
-        # first_state_index = improved_map_state(s1)
-        # second_state_index = improved_map_state(s2)
-        # if end:
-        #     self.q_table[second_state_index] = [r, r]
-        # old_value = self.q_table[first_state_index][a]
-        # self.q_table[first_state_index][a] = (1-self.learning_rate)*old_value + self.learning_rate*(
-        #     r + self.discount_factor*max(self.q_table[second_state_index]))
-        self.replayMem.append((s1, a, r, s2, isEnd))
+        """ this function is called during training on each step of the game where
+            the state transition is going from state s1 with action a to state s2 and
+            yields the reward r. If s2 is a terminal state, end==True, otherwise end==False.
 
-        # showState(s1)
-        # showState(s2)
+            Unless a terminal state was reached, two subsequent calls to observe will be for
+            subsequent steps in the same episode. That is, s1 in the second call will be s2
+            from the first call.
+            """
+        self.replayMem.append((s1, a, r, s2, isEnd))
 
         if len(self.replayMem) > self.replayMemMaxSize:
             self.replayMem.popleft()
@@ -144,15 +98,12 @@ class FlappyDeepQAgent(FlappyAgent):
 
 
     def training_policy(self, state):
-        # print("state: %s" % state)
-        # p = random.random()
-        # if p < self.epsilon:
-        #     return random.randint(0, 1)
-        # else:
-        #     stateIndex = improved_map_state(state)
-        #     return np.argmax(self.q_table[stateIndex])
+        """ Returns the index of the action that should be done in state while training the agent.
+            Possible actions in Flappy Bird are 0 (flap the wing) or 1 (do nothing).
+
+            training_policy is called once per frame in the game while training
+        """
         if random.random() < self.epsilon:
-            # print("Taking random action")
             retval = random.randint(0,1)
         else:
             q = self.QNetwork.predict(state)
@@ -163,14 +114,16 @@ class FlappyDeepQAgent(FlappyAgent):
                     self.printPredictionCnt = 500
             self.printPredictionCnt -= 1
             retval = np.argmax(q)
-        # print("Taking action: {}".format(retval))
         return retval
 
 
     def policy(self, state):
-        # print("state: %s" % state)
-        # stateIndex = improved_map_state(state)
-        # return np.argmax(self.q_table[stateIndex])
+        """ Returns the index of the action that should be done in state when training is completed.
+            Possible actions in Flappy Bird are 0 (flap the wing) or 1 (do nothing).
+
+            policy is called once per frame in the game (30 times per second in real-time)
+            and needs to be sufficiently fast to not slow down the game.
+        """
         q = self.QNetwork.predict(state)
         return np.argmax(q)
 
@@ -253,7 +206,7 @@ def run_game(agent, train):
         dispScreen = False
         force_fps = True
     else:
-        reward_values = reward_structures["game"]
+        reward_values = reward_structures["ActualGame"]
         dispScreen = True
         force_fps = False
 
